5a6745d90c1632ca7ee147c4824d4f4be0cccd09
deboramartins15 <deehmartin@gmail.com>
2020-09-28 18:26:46 -0300

    :recycle: (frontend) Ajustes do CSS da pagina de Dashboard

--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -22,18 +22,16 @@ export const TableWrapper = styled.div`
   width: 90%;
   height: 100%;
   display: flex;
-  flex-direction:column;
-  justify-content: center;
-  /* max-height: 470px;   */
-  /* overflow-y: auto; */
-  margin-top: 16px;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px auto;
 
   .form-group{
-    width: 418px;
-    margin-left: ${(props) => props.marginInput}
+    width: 205px;
   }
 
-  @media (max-width: 360px) {
-    margin-top: 100px;
+  .container{
+    margin: 0;
+    padding:0;
   }
 `;
